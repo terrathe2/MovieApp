e2855602172efff0f85309a9c50e296460b34e93
terrathe2 <putra.redha@gmail.com>
2023-06-01 17:35:23 +0700

    setup navigation and base fragment

---
+++ app/src/main/java/com/redhaputra/movieapp/common/ui/extensions/AutoClearBinding.kt
@@ -0,0 +1,65 @@
+package com.redhaputra.movieapp.common.ui.extensions
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ * A lazy property that gets cleaned up when the fragment's view is destroyed.
+ *
+ * Accessing this variable while the fragment's view is destroyed will throw NPE.
+ */
+class AutoClearBinding<T : Any>(val fragment: Fragment) : ReadWriteProperty<Fragment, T> {
+    private var _value: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            val viewLifecycleOwnerLiveDataObserver =
+                Observer<LifecycleOwner?> {
+                    val viewLifecycleOwner = it ?: return@Observer
+
+                    viewLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+                        override fun onDestroy(owner: LifecycleOwner) {
+                            _value = null
+                        }
+                    })
+                }
+
+            override fun onCreate(owner: LifecycleOwner) {
+                fragment.viewLifecycleOwnerLiveData.observeForever(
+                    viewLifecycleOwnerLiveDataObserver
+                )
+            }
+
+            override fun onDestroy(owner: LifecycleOwner) {
+                fragment.viewLifecycleOwnerLiveData.removeObserver(
+                    viewLifecycleOwnerLiveDataObserver
+                )
+            }
+        })
+    }
+
+    /**
+     * Clear the binding value. Set as null
+     */
+    fun clearValue() {
+        _value = null
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>) =
+        _value ?: error(
+            "should never call auto-cleared-value get when it might not be available"
+        )
+
+    override fun setValue(thisRef: Fragment, property: KProperty<*>, value: T) {
+        _value = value
+    }
+}
+
+/**
+ * Creates an [AutoClearBinding] associated with this fragment.
+ */
+fun <T : Any> Fragment.autoClearBinding() = AutoClearBinding<T>(this)
---
+++ app/src/main/java/com/redhaputra/movieapp/screen/detail/DetailMovieFragment.kt
@@ -0,0 +1,13 @@
+package com.redhaputra.movieapp.screen.detail
+
+import com.redhaputra.movieapp.R
+import com.redhaputra.movieapp.common.ui.base.BaseFragment
+import com.redhaputra.movieapp.databinding.FragmentDetailMovieBinding
+
+/**
+ * Detail Movie Screen [BaseFragment] subclass
+ * to show movie detail.
+ */
+class DetailMovieFragment :
+    BaseFragment<FragmentDetailMovieBinding>(R.layout.fragment_detail_movie) {
+}
